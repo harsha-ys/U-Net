7e4bf1748842b6931c36651785683625416babc2
harsha-ys <hyoshith@gmail.com>
2022-12-31 03:46:21 +0530

    add a dropout to the convolution layer to omit some neurons from the model randomly so thus reduce the correlation between the features eventually increase the generalization of th emodel

--- unet.py
+++ unet.py
@@ -15,7 +15,8 @@ IMG_chanel = 3
 #input layer
 inputs = tf.keras.layers.Input((IMG_width, IMG_height, IMG_chanel))
 
-s= tf.keras.layers.Lambda(lambda x:x/255)(inputs)
+s= tf.keras.layers.Lambda(lambda x:x/255)(inputs)#convert integer rgb values into floats within the range 0-1
 
 c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+c1 = tf.keras.layers.Dropout(0.1)(c1)#drop out to generalize
 
