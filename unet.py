efd0a44900026d1f37e931b56337e0e4a67525e2
harsha-ys <hyoshith@gmail.com>
2023-01-02 14:07:26 +0530

    contradiction path

--- unet.py
+++ unet.py
@@ -17,9 +17,33 @@ inputs = tf.keras.layers.Input((IMG_width, IMG_height, IMG_chanel))
 
 s= tf.keras.layers.Lambda(lambda x:x/255)(inputs)#convert integer rgb values into floats within the range 0-1
 
+
+#contradiction path(Encoder path)
 c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
 c1 = tf.keras.layers.Dropout(0.1)(c1)#drop out to generalize
 c1 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
 p1 = tf.keras.layers.MaxPool2D((2,2))(c1)
+
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+c2 = tf.keras.layers.Dropout(0.1)(c2)#drop out to generalize
+c2 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+p2 = tf.keras.layers.MaxPool2D((2,2))(c2)
+
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+c3 = tf.keras.layers.Dropout(0.2)(c3)#drop out to generalize
+c3 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+p3 = tf.keras.layers.MaxPool2D((2,2))(c3)
+
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+c4 = tf.keras.layers.Dropout(0.2)(c4)#drop out to generalize
+c4 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+p4 = tf.keras.layers.MaxPool2D((2,2))(c4)
+
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+c5 = tf.keras.layers.Dropout(0.2)(c5)#drop out to generalize
+c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+#p5 = tf.keras.layers.MaxPool2D((2,2))(c1)
+
+
 
 
