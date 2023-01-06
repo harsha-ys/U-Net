84a2fe4b02b9948fe164823b0e86ac9b5007fd10
harsha-ys <hyoshith@gmail.com>
2023-01-06 19:51:43 +0530

    expansive path

--- unet.py
+++ unet.py
@@ -44,6 +44,37 @@ c5 = tf.keras.layers.Dropout(0.2)(c5)#drop out to generalize
 c5 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
 #p5 = tf.keras.layers.MaxPool2D((2,2))(c1)
 
+#expansive path
+u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2,2), padding="same")(c5)
+u6 = tf.keras.layers.concatenate([u6, c4])
+c6 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+c6 = tf.keras.layers.Dropout(0.2)(c6)
+c6 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2,2), padding="same")(c6)
+u7 = tf.keras.layers.concatenate([u7, c3])
+c7 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+c7 = tf.keras.layers.Dropout(0.2)(c7)
+c7 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2,2), padding="same")(c7)
+u8 = tf.keras.layers.concatenate([u8, c2])
+c8 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+c8 = tf.keras.layers.Dropout(0.2)(c8)
+c8 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2,2), padding="same")(c8)
+u9 = tf.keras.layers.concatenate([u9, c1])
+c9 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+c9 = tf.keras.layers.Dropout(0.2)(c9)
+c9 = tf.keras.layers.Conv2D(16, (3,3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+
+outputs = tf.keras.layers.Conv2D(1, (1,1), activation='sigmoid')(c9) 
+
+
+
+
 
 #Expansive path (Decoder path)
 
